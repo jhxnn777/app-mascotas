e5aa4c8ce2aa2c2472bed36599bb57289930b220
jhxnnramirez777@gmail.com <ramirezjhon13579@gmail.com>
2026-07-23 22:29:14 -0400

    feat: se Agrego el GET para listar mascotas

---
+++ src/components/MascotaList.jsx
@@ -0,0 +1,53 @@
+import { Link } from "react-router-dom";
+
+function MascotaList({ lista }) {
+    return (
+        <section>
+            {/* Recorro la lista de mascotas que recibo por props */}
+            {lista.map(mascota => (
+                // Muestro una mascota por cada vuelta del map
+                <article key={mascota.id}>
+                    <h2>
+                        {mascota.nombre}
+                    </h2>
+                    {/* Solo muestro la imagen si la mascota tiene una */}
+                    {mascota.imagen && (
+                        <img
+                            src={mascota.imagen}
+                            alt={mascota.nombre}
+                            width="200"
+                        />
+                    )}
+
+                    <p>
+                        Descripción: {mascota.descripcion}
+                    </p>
+
+                    <p>
+                        Estado: {mascota.estado}
+                    </p>
+
+                    <p>
+                        Tipo: {mascota.tipo_animal}
+                    </p>
+
+                    <p>
+                        Edad: {
+                            mascota.edad !== null
+                                ? mascota.edad
+                                : "No especificada"
+                        }
+                    </p>
+
+                    {/* Este link me lleva al detalle de la mascota usando su ID */}
+                    <Link to={`/mascotas/${mascota.id}`}>
+                        Ver detalle
+                    </Link>
+                    <hr />
+                </article>
+            ))}
+        </section>
+    );
+}
+
+export default MascotaList;
--- src/pages/MascotasPage.jsx
+++ src/pages/MascotasPage.jsx
@@ -1,5 +1,49 @@
+import { useEffect, useState } from "react";
+import MascotaList from "../components/MascotaList";
+import api from "../api/api";
+
 function MascotasPage() {
-    return <h1>Listado de Mascotas</h1>;
+
+    // aca voy a guardar la lista de mascotas que viene de la API
+    const [mascotas, setMascotas] = useState([]);
+    // aca guardo un mensaje si ocurre algún error
+    const [error, setError] = useState("");
+    // funcion para traer las mascotas desde la API
+    const fetchMascotas = async () => {
+        try {
+            // hago una petición GET para obtener todas las mascotas
+            const response = await api.get("mascotas/");
+            // si la petición salió bien guardo los datos
+            if (response.status === 200) {
+                setMascotas(response.data);
+            }
+        } catch (error) {
+            // muestro en consola el codigo y los datos del error
+            console.log(error.response?.status);
+            console.log(error.response?.data);
+            // si es error 404 muestra un mensaje 
+            if (error.response?.status === 404) {
+                setError("No se encontraron las mascotas");
+            } else {
+                setError("Error al cargar las mascotas");
+            }
+        }
+    };
+
+    // se ejecuta una vez cuando se carga la página
+    useEffect(() => {fetchMascotas();}, []);
+
+    return (
+        <main>
+            <h1>Listado de Mascotas</h1>
+            {/* si hay error lo muestro si no envio la lista al componente */}
+            {error ? (
+                <p>{error}</p>
+            ) : (
+                <MascotaList lista={mascotas} />
+            )}
+        </main>
+    );
 }
 
 export default MascotasPage;
