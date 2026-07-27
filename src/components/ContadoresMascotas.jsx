6ca70f871602ff93a0fe80a922a27d13606cb061
jhxnnramirez777@gmail.com <ramirezjhon13579@gmail.com>
2026-07-27 00:26:48 -0400

    feat: instale  lucide-react y agrege iconos a los contadores

--- src/components/ContadoresMascotas.jsx
+++ src/components/ContadoresMascotas.jsx
@@ -1,3 +1,11 @@
+import {
+    PawPrint,
+    CircleAlert,
+    Search,
+    Heart,
+    CircleCheck
+} from "lucide-react";
+
 function ContadoresMascotas({ mascotas }) {
     // Cuento todas las mascotas registradas
     const total = mascotas.length;
@@ -23,35 +31,50 @@ function ContadoresMascotas({ mascotas }) {
         <section className="row g-3 mb-4">
             <div className="col-6 col-md">
                 <div className="contador-card contador-total">
-                    <span>Total</span>
+                    <span className="d-flex align-items-center gap-2">
+                        <PawPrint size={20} />
+                        Total
+                    </span>
                     <strong>{total}</strong>
                 </div>
             </div>
 
             <div className="col-6 col-md">
                 <div className="contador-card contador-perdidas">
-                    <span>Perdidas</span>
+                    <span className="d-flex align-items-center gap-2">
+                        <CircleAlert size={20} />
+                        Perdidas
+                    </span>
                     <strong>{perdidas}</strong>
                 </div>
             </div>
 
             <div className="col-6 col-md">
                 <div className="contador-card contador-encontradas">
-                    <span>Encontradas</span>
+                    <span className="d-flex align-items-center gap-2">
+                        <Search size={20} />
+                        Encontradas
+                    </span>
                     <strong>{encontradas}</strong>
                 </div>
             </div>
 
             <div className="col-6 col-md">
                 <div className="contador-card contador-adopcion">
-                    <span>En adopción</span>
+                    <span className="d-flex align-items-center gap-2">
+                        <Heart size={20} />
+                        En adopción
+                    </span>
                     <strong>{enAdopcion}</strong>
                 </div>
             </div>
 
             <div className="col-6 col-md">
                 <div className="contador-card contador-adoptadas">
-                    <span>Adoptadas</span>
+                    <span className="d-flex align-items-center gap-2">
+                        <CircleCheck size={20} />
+                        Adoptadas
+                    </span>
                     <strong>{adoptadas}</strong>
                 </div>
             </div>
